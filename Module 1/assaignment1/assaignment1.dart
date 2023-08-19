8155c2af4dea8fc072d41dd827b7b023cce86428
ARAFAT <99494874+ARAFAT2000@users.noreply.github.com>
2023-08-19 12:14:57 +0600

    Add files via upload

---
+++ Module 1/assaignment1/assaignment1.dart
@@ -0,0 +1,16 @@
+void main() {
+  const int a = 7;
+  const int b = 3;
+
+  int add = a + b;
+  int sub = a - b;
+  int mul = a * b;
+  double div = a / b;
+  int mod = a % b;
+
+  print('Addition: $a + $b = $add');
+  print('Subtraction: $a + $b = $sub');
+  print('Multiplication: $a + $b = $mul');
+  print('Division: $a + $b = $div');
+  print('Modulus: $a + $b = $mod');
+}
